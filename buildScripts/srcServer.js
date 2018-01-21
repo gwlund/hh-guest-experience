41a0d45ca7e023c8a5cb80e4e3a5b3e701179f11
Gil Lund <gwlund71@gmail.com>
2018-01-21 09:44:26 -0800

    Initial check-in.  Working tabulator example

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.use(express.static('data'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/tabulator.html'));
+})
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+})
